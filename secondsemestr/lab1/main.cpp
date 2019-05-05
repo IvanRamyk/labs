8a411d810aebd7abde1145b2724c3400219508e2
IvanRamyk <44612098+IvanRamyk@users.noreply.github.com>
2019-05-05 16:27:23 +0300

    Create main.cpp

---
+++ secondsemestr/lab1/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Date{
+    int year;
+    int month;
+    int day;
+};
+
+struct Time{
+    int hours;
+    int minutes;
+};
+
+struct Product{
+    string name;
+    string units;
+    int amount;
+    Date date;
+    Time time;
+    int shelf_life;
+};
+
+struct Vector(){
+
+}
+
+int main()
+{
+
+    return 0;
+}
