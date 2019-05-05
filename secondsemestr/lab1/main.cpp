ca25e886070e9dc1f370e1b9d99f2be0c53b983c
IvanRamyk <44612098+IvanRamyk@users.noreply.github.com>
2019-05-05 18:40:58 +0300

    Update main.cpp

--- secondsemestr/lab1/main.cpp
+++ secondsemestr/lab1/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <fstream>
 
 
 using namespace std;
@@ -11,6 +12,15 @@ struct Date{
     int day;
     void print(){
         cout << day << "." << month << "." << year << "\n";
+    }
+    void get(){
+        cin >> day >> month >> year;
+    }
+    void in(ifstream fin){
+        fin >> day >> month >> year;
+    }
+    void out(ofstream fout){
+        fout << day << " " << month << " " << year << " ";
     }
 };
 bool operator <=(Date a, Date b){
@@ -23,6 +33,12 @@ struct Time{
     int minutes;
     void print(){
         cout << hours << ":" << minutes << "\n";
+    }
+    void get(ifstream fin){
+        fin >> hours >> minutes;
+    }
+    void out(ofstream fout){
+        fout << hours << " " << minutes << " ";
     }
 };
 
@@ -41,14 +57,33 @@ struct Product{
         time.print();
         cout << "shelf life - " << shelf_life << "\n";
     }
-    void get(){
-
+    bool get(){
+        cin >> name >> amount >> units;
+        cin >> date.day >> date.month >> date.year;
+        cin >> time.hours >> time.minutes;
+        if (cin >> shelf_life) return 1;
+        return 0;
+    }
+    bool in(ifstream& fin){
+        fin >> name >> amount >> units;
+        fin >> date.day >> date.month >> date.year;
+        fin >> time.hours >> time.minutes;
+        if (fin >> shelf_life) return 1;
+        return 0;
+    }
+    void out(ofstream& fout){
+        fout << name << " " << amount << " " << units << " ";
+        fout << date.day << " " << date.month << " " << date.year << " ";
+        fout << time.hours << " " << time.minutes << " ";
+        fout << shelf_life << "\n";
     }
 };
 
 struct Vector{
     vector <Product> products;
-    void get(Product temp){
+    void get(){
+        Product temp;
+        temp.get();
         temp.id = products.size();
         products.push_back(temp);
     }
@@ -89,16 +124,41 @@ struct Vector{
 };
 
 struct File{
-    void get(Product temp){
-        freopen("struct", "r", stdin);
+    void get(){
+        Product to_ad;
+        to_ad.get();
+        string file = "struct";
+        ifstream fin("struct");
         vector <Product> temp;
         Product product;
-        while ()
+        while (product.in(fin))
+            temp.push_back(product);
+        fin.close();
+        ofstream fout("struct");
+        for (auto i : temp)
+            i.out(fout);
+        to_ad.out(fout);
+        fout.close();
+    }
+    void print(){
+        vector <Product> temp;
+        Product product;
+        ifstream fin("struct");
+        while (product.in(fin))
+            temp.push_back(product);
+        fin.close();
+        for (auto i : temp)
+            i.print();
     }
 };
 
 int main()
 {
-
+    File str;
+    /*int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+        str.get();*/
+    str.print();
     return 0;
 }
