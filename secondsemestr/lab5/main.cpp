37f0ff3db596340955298d34c7046ef417e88360
IvanRamyk <44612098+IvanRamyk@users.noreply.github.com>
2019-05-12 18:39:16 +0300

    Update main.cpp

--- secondsemestr/lab5/main.cpp
+++ secondsemestr/lab5/main.cpp
@@ -63,7 +63,7 @@ struct graf_adj{
         orient = 0;
     }
     graf_adj(int v, bool o = 0){
-        orient = 0;
+        orient = o;
         number_vertex = v;
         adj.resize(number_vertex);
     }
@@ -147,47 +147,105 @@ vector <int> dijkstra(graf_adj graf, int v){
     return dist;
 }
 
-int n; // число вершин
-vector<int> g[MAXN]; // граф
-bool used[MAXN];
-vector<int> ans;
-
-void dfs (int v) {
+void top_dfs (graf_adj &graf, int v, vector <bool>& used, vector <int> &ans) {
     used[v] = true;
-    for (size_t i=0; i<g[v].size(); ++i) {
-        int to = g[v][i];
-        if (!used[to])
-            dfs (to);
+    for (auto i : graf.adj[v]){
+        if (!used[i.to]) {
+            top_dfs(graf, i.to, used, ans);
+        }
     }
-    ans.push_back (v);
+    cout << v << "\n";
+    ans.push_back(v);
 }
 
-void topological_sort() {
+vector <int> topological_sort(graf_adj graf) {
+    int n = graf.number_vertex;
+    vector <bool> used;
+    used.resize(n);
     for (int i=0; i<n; ++i)
         used[i] = false;
-    ans.clear();
+    vector <int> result;
     for (int i=0; i<n; ++i)
         if (!used[i])
-            dfs (i);
-    reverse (ans.begin(), ans.end());
+            top_dfs (graf, i, used, result);
+    reverse (result.begin(), result.end());
+    return result;
 }
+
+struct span_edge{
+    int from;
+    int to;
+    int weight;
+};
+bool operator <(span_edge a, span_edge b){
+    return a.weight > b.weight;
+}
+
+void spanning_dfs(graf_adj &graf, int v, vector <bool> &used, vector <span_edge> &result){
+    used[v] = 1;
+    for (auto i : graf.adj[v])
+        if (!used[i.to]){
+            result.push_back({v, i.to, i.weight});
+            spanning_dfs(graf, i.to, used, result);
+        }
+}
+
+pair <vector <span_edge> , int > spanning_tree(graf_adj graf){
+    vector <span_edge> result;
+    vector <bool> used;
+    used.resize(graf.number_vertex);
+    spanning_dfs(graf, 0, used, result);
+    int weight = 0;
+    for (auto i : result)
+        weight += i.weight;
+    return {result, weight};
+}
+
+pair <vector <span_edge> , int > minimal_spanning_tree(graf_adj graf){
+    vector <span_edge> result;
+    vector <int> dist;
+    vector <int> used;
+    dist.resize(graf.number_vertex);
+    for (int i = 0; i < graf.number_vertex; ++i) {
+        dist[i] = -1;
+        used[i] = 0;
+    }
+    dist[0] = 0;
+    priority_queue <span_edge> q;
+    q.push({0, 0, 0});
+    while (!q.empty()){
+        result.push_back(q.top());
+        int cur = q.top().to;
+        used[cur] = 1;
+        q.pop();
+        for (auto i : graf.adj[cur])
+            if ((dist[i.to] == -1 || dist[i.to] > i.weight) && !used[i.to]){
+                dist[i.to] = i.weight;
+                q.push({cur, i.to, i.weight});
+            }
+    }
+    result.erase(result.begin());
+    int total = 0;
+    for (auto i : result)
+        total += i.weight;
+    return {result, total};
+}
+
 
 int main() {
     int n;
     cin >> n;
-    graf_adj graf = graf_adj(n);
+    graf_adj graf = graf_adj(n, 0);
     int m;
     cin >> m;
     for (int i = 0; i < m; ++i){
-        int a, b;
-        cin >> a >> b;
-        graf.add(a, b);
+        int a, b, w;
+        cin >> a >> b >> w;
+        graf.add(a, b, w);
     }
-    int v;
-    cin >> v;
-    vector <int> dist = bfs(graf, v);
-    for (auto i : dist){
-        cout << i << " ";
+    vector <span_edge> ans = minimal_spanning_tree(graf).first;
+    for (auto i : ans){
+        cout << i.from << " " << i.to << " " << i.weight << "\n";
     }
     return 0;
 }
