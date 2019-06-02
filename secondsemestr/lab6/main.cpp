32f45edb3653126fa3554f6acf16f409bb344c30
Ivan Ramyk <ivan.ramyk.official@gmail.com>
2019-06-02 23:37:33 +0300

    Update main.cpp

--- secondsemestr/lab6/main.cpp
+++ secondsemestr/lab6/main.cpp
@@ -46,36 +46,38 @@ struct Date{
 
 bool operator <(Date a, Date b ){
     return (a.year * 100 + a.month) * 20 + a.day < (b.year * 100 + b.month) * 20 + b.day ||
-    ((a.year * 100 + a.month) * 20 + a.day == (b.year * 100 + b.month) * 20 + b.day && a.time < b.time);
+           ((a.year * 100 + a.month) * 20 + a.day == (b.year * 100 + b.month) * 20 + b.day && a.time < b.time);
 }
+
 bool operator <=(Date a, Date b ){
     return (a.year * 100 + a.month) * 20 + a.day <= (b.year * 100 + b.month) * 20 + b.day ||
            ((a.year * 100 + a.month) * 20 + a.day == (b.year * 100 + b.month) * 20 + b.day && a.time <= b.time);
 }
+
 bool operator !=(Date a, Date b){
     return (a.year != b.year) || (a.month != b.month) || (a.day != b.day) || (a.time != b.time);
 }
 
-struct Node{
-    Node *next;
-    Node *prev;
+struct node{
+    node *next;
+    node *prev;
     Date date;
-    Node(){
+    node(){
         date = Date();
         next = nullptr;
         prev = nullptr;
     }
-    Node(Date _date, Node *n = nullptr, Node *p = nullptr){
+    node(Date _date, node *n = nullptr, node *p = nullptr){
         date = _date;
         next = n;
         prev = p;
     }
 };
 
-struct List{
-    Node * head;
-    Node * tail;
-    List(){
+struct list{
+    node * head;
+    node * tail;
+    list(){
         head = nullptr;
         tail = nullptr;
     }
@@ -84,27 +86,27 @@ struct List{
     }
     void add(Date date){
         if (!head){
-            head = tail = new Node(date);
+            head = tail = new node(date);
         }
         else {
             if (date <= head->date) {
-                Node *new_node = new Node(date, head);
+                node *new_node = new node(date, head);
                 head->prev = new_node;
                 head = new_node;
             }
             else {
-                Node * current_ptr = head;
+                node * current_ptr = head;
                 while (!(date < current_ptr->date)){
                     if (current_ptr->next == nullptr) break;
                     current_ptr = current_ptr->next;
                 }
                 if (current_ptr == tail && (current_ptr->date < date)){
-                    Node *new_node = new Node(date, nullptr, tail);
+                    node *new_node = new node(date, nullptr, tail);
                     tail->next = new_node;
                     tail = new_node;
                 }
                 else {
-                    Node *new_node = new Node(date, current_ptr, current_ptr->prev);
+                    node *new_node = new node(date, current_ptr, current_ptr->prev);
                     current_ptr->prev->next = new_node;
                     current_ptr->prev = new_node;
                 }
@@ -112,14 +114,14 @@ struct List{
         }
     }
     void delete_date(Date date){
-        Node * current_node = head;
+        node * current_node = head;
         while (current_node != nullptr && current_node->date != date)
             current_node = current_node->next;
         if (current_node != nullptr){
-            Node * begin = current_node->prev;
+            node * begin = current_node->prev;
             while (current_node != nullptr && !(current_node->date != date))
                 current_node = current_node->next;
-            Node * end = current_node;
+            node * end = current_node;
             if (begin == nullptr){
                 if (end == nullptr){
                     head = tail = nullptr;
@@ -144,15 +146,15 @@ struct List{
         }
     }
     void print(){
-        Node *cur =  head;
+        node *cur =  head;
         while (cur != nullptr){
             cur->date.print();
             cur = cur->next;
         }
     }
-    std::vector <Node *> find(Date min_value, Date max_value){
-        std::vector <Node *> result;
-        Node * current_node = head;
+    std::vector <node *> find(Date min_value, Date max_value){
+        std::vector <node *> result;
+        node * current_node = head;
         while (current_node != nullptr){
             if (min_value <= current_node->date && current_node->date <= max_value)
                 result.push_back(current_node);
@@ -160,11 +162,10 @@ struct List{
         }
         return result;
     }
-    std::vector <Node *> find(Date value){
+    std::vector <node *> find(Date value){
         return find(value, value);
     }
 };
-
 
 struct binary_tree_node{
     Date date;
@@ -239,8 +240,10 @@ struct binary_tree{
         return answer;
     }
     binary_tree_node * delete_member_node(binary_tree_node * current_node, Date min_value, Date max_value){
-        if (!current_node) return current_node;
+        if (current_node == nullptr) return current_node;
         if (min_value <= current_node->date && current_node->date <= max_value){
+            current_node->left_child = delete_member_node(current_node->left_child, min_value, max_value);
+            current_node->right_child = delete_member_node(current_node->right_child, min_value, max_value);
             if (!current_node->right_child){
                 binary_tree_node * temp = current_node->left_child;
                 delete current_node;
@@ -268,25 +271,136 @@ struct binary_tree{
     void delete_member(Date min_value, Date max_value){
         root = delete_member_node(root, min_value, max_value);
     }
+    void delete_member(Date value){
+        root = delete_member_node(root, value, value);
+    }
+    void add_random(int cnt_random){
+        for (int i = 0;i <  cnt_random; ++i){
+            Date temp = Date();
+            add(temp);
+        }
+    }
+};
+
+const int list_size = 100;
+
+struct array_list{
+    Date dates[list_size];
+    int cnt_dates_in_list;
+    array_list(){
+        cnt_dates_in_list = 0;
+    }
+    void add(Date date){
+        if (cnt_dates_in_list == 0){
+            dates[0] = date;
+            cnt_dates_in_list++;
+            return;
+        }
+        int left = 0;
+        int right = cnt_dates_in_list;
+        while (right - left > 1){
+            int middle = (right + left) >> 1;
+            if (date < dates[middle])
+                right = middle;
+            else
+                left = middle;
+        }
+        int pos;
+        if (date < dates[left])
+            pos = left;
+        else
+            pos = left + 1;
+        for (int i = cnt_dates_in_list; i > pos; --i)
+            dates[i] = dates[i - 1];
+        dates[pos] = date;
+        cnt_dates_in_list++;
+    }
+    void print_in_sort_order(){
+        for (int i = 0; i < cnt_dates_in_list; ++i)
+            dates[i].print();
+    }
+    void delete_date(Date date){
+        int left = 0;
+        int right = cnt_dates_in_list;
+        while (right - left > 1){
+            int middle = (right + left) >> 1;
+            if (date <= dates[middle])
+                right = middle;
+            else
+                left = middle;
+        }
+        int pos = left;
+        while (pos < cnt_dates_in_list && dates[pos] < date)
+            ++pos;
+        int begin = pos;
+        while (pos < cnt_dates_in_list && (!(date < dates[pos])))
+            pos++;
+        int end = pos;
+        int cnt_delete = end - begin;
+        if (cnt_delete == 0) return;
+        for (int i = begin; i < cnt_dates_in_list - cnt_delete; ++i)
+            dates[i] = dates[i + cnt_delete];
+        cnt_dates_in_list -= cnt_delete;
+    }
+    std::vector <Date> find(Date min_value, Date max_value){
+        std::vector<Date> result;
+        int left = 0;
+        int right = cnt_dates_in_list;
+        while (right - left > 1){
+            int middle = (right + left) >> 1;
+            if (min_value <= dates[middle])
+                right = middle;
+            else
+                left = middle;
+        }
+        int L;
+        if (min_value <= dates[left])
+            L = left;
+        else
+            L = left + 1;
+        left = 0;
+        right = cnt_dates_in_list;
+        while (right - left > 1){
+            int middle = (right + left) >> 1;
+            if (min_value < dates[middle])
+                right = middle;
+            else
+                left = middle;
+        }
+        int R = left;
+        for (int i = L; i <= R; ++i)
+            result.push_back(dates[i]);
+        return result;
+    }
+    std::vector<Date>find(Date value){
+        return find(value, value);
+    }
+    void add_random(int cnt_random){
+        for (int i = 0;i <  cnt_random; ++i){
+            Date temp = Date();
+            add(temp);
+        }
+    }
 };
 
 int main() {
     srand(time(0));
-    List _list = List();
-    Date del = Date();
-    _list.add(del);
+    array_list _tree = array_list();
+    Date del;
     for (int i = 0; i < 10; ++i) {
         Date temp = Date();
-        _list.add(temp);
-        _list.add(temp);
+        _tree.add(temp);
+        _tree.add(temp);
         del = temp;
+        temp.print();
     }
     std::cout << "\n";
-    _list.print();
+    _tree.print_in_sort_order();
     std::cout << "\n";
-    _list.delete_date(del);
-    del.print();
+    std::vector<Date> vec = _tree.find(del);
+    for (auto i : vec)
+        i.print();
     std::cout << "\n";
-    _list.print();
+    _tree.print_in_sort_order();
     return 0;
 }
