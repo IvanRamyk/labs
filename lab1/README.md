0a8ab4aae90d0180245cc368b04181081c6da304
Ivan Ramyk <ivan.ramyk@gmail.com>
2019-10-17 14:22:02 +0300

    README update

---
+++ lab1/README.md
@@ -0,0 +1,47 @@
+# Lab1 
+8 from List1 (Graph and Tree)
+6 from List2 (Set of dices)
+
+List1:
+
+	Class Graph 
+	methods:
+		1. add vertex
+		2. add edge
+		3. search connected components 
+		4. minimum spanning tree (returns Graph)
+		5. topological sort
+		6. print 
+
+	Class Tree 
+	methods:
+		1. convert Graph to Tree
+		2. print
+		3. add vertex
+
+	Basic level - ****
+	Extra - * (minimum spanning tree)
+	Total - *****
+
+List2:
+	Class Dice
+	methods:
+		1. add side
+		2. check valid
+		3. print
+		4. create random dice
+
+	Class SetDices methods:
+		1. add dice
+		2. print
+		3. get probability for each sum
+		4. expected sum
+		5. overloaded operation < (by expected sum)	 
+	Basic level - ***
+	Extra - * overloaded operation <
+	Total - ****
+Also all methods was tested via googletest
+All tests are in directory tests
+I hope for additional points :)  	
+Total count - 9	
+
--- lab1/tests/test_graph.cpp
+++ lab1/tests/test_graph.cpp
@@ -13,9 +13,10 @@ TEST(TestGraphStandartTypes, TestGraphInit){
     graph.add_vertex("Kyiv");
     graph.add_vertex("Lviv");
     graph.add_vertex("Irpen");
-    graph.add_edge(v1, v2, edge);
-    graph.add_edge("Lviv", "Irpen", 13);
-    graph.add_edge("Kyiv", "Irpen", 12);
+    EXPECT_TRUE(graph.add_edge(v1, v2, edge));
+    EXPECT_TRUE(graph.add_edge("Lviv", "Irpen", 13));
+    EXPECT_TRUE(graph.add_edge("Kyiv", "Irpen", 12));
+    EXPECT_TRUE(!graph.add_edge("Kyiv", "abc", 1));// we can't add this edge because no vertex "abc"
 }
 
 TEST(TestGraphStandartTypes, TestGraphComponents){
@@ -32,5 +33,22 @@ TEST(TestGraphStandartTypes, TestGraphComponents){
     graph.add_edge("Kyiv", "Irpen", 12);
     vector <vector <std::string>> components = graph.search_component();
     EXPECT_EQ(2, components.size());
-    //for (int i = 0; i < )
+    EXPECT_EQ(components[0][0], "Kyiv");
+    EXPECT_EQ(components[0][1], "Lviv");
+    EXPECT_EQ(components[0][2], "Irpen");
+    EXPECT_EQ(components[1][0], "Kirov");
+}
+
+TEST(TestGraphStandartTypes, TestGraphSpanningTree){
+    Graph<std::string, int> graph;
+    std::string v1 = "Kyiv";
+    std::string v2 = "Lviv";
+    int edge = 145;
+    graph.add_vertex("Kyiv");
+    graph.add_vertex("Lviv");
+    graph.add_vertex("Irpen");
+    EXPECT_TRUE(graph.add_edge(v1, v2, edge));
+    EXPECT_TRUE(graph.add_edge("Lviv", "Irpen", 13));
+    EXPECT_TRUE(graph.add_edge("Kyiv", "Irpen", 12));
+    EXPECT_TRUE(!graph.add_edge("Kyiv", "abc", 1));// we can't add this edge because no vertex "abc"
 }
